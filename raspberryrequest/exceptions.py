e9de1ce9c1b171afe94d773bf27680968a02de41
Charlie Marshall <charlie.marshall1996@gmail.com>
2023-10-17 22:57:21 +0100

    Update exceptions.py

--- raspberryrequest/exceptions.py
+++ raspberryrequest/exceptions.py
@@ -1,17 +1,17 @@
 from typing import Optional
 
 
-class RaspberryRequestError(Exception):
+class RaspberryRequestException(Exception):
     def __init__(self, message: Optional[str] = None):
         super().__init__(message or 'Error in RaspberryRequest.')
 
 
-class MaxRetryError(RaspberryRequestError):
+class MaxRetryError(RaspberryRequestException):
     def __init__(self, message: Optional[str] = None):
         super().__init__(message or 'Maximum number of request retries reached.')
 
 
-class StatusCodeError(RaspberryRequestError):
+class StatusCodeError(RaspberryRequestException):
     def __init__(self, message: Optional[str] = None):
         super().__init__(message or 'Error in status code.')
 
